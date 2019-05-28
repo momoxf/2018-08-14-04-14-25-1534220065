238948d28eaf46a54ccaf6a69e9b7cfad8d46cd7
momoxf <51114437+momoxf@users.noreply.github.com>
2019-05-28 23:08:12 +0800

    Update main.js

--- main/main.js
+++ main/main.js
@@ -1,4 +1,19 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+ const      lednum  =   [["._.","...","._.","._.","...","._.","._.","._.","._.","._."],
+                        ["|.|","..|","._|","._|","|_|","|_.","|_.","..|","|_|","|_|"],
+                        ["|_|","..|","|_.","._|","..|","._|","|_|","..|","|_|","..|"]]
+
+module.exports = function main(str) {
+  const out = [[], [], []]
+  
+  for(let s of str){
+    out[0].push(lednum[0][s])
+    out[1].push(lednum[1][s])
+    out[2].push(lednum[2][s])
+      
+  }
+  return out.mop(row => row.join('')+'\n').join('')
+   
 };
+ 
+
+
